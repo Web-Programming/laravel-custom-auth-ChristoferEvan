bc56633536d45ac7bd25368066290016e32ebc28
Christofer Evan <evan200421@gmail.com>
2023-11-22 12:53:08 +0700

    CustomAuthentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
+    
     /**
      * The application's route middleware groups.
      *
@@ -60,9 +61,10 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+       // tambahkan middleware alias dengan nama cek_login
+        'cek_login'=> \App\Http\Middleware\Cek_login::class
     ];
 }
